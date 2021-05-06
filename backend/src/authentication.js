d60469cf2342a3fe89bcc0f6b1cb6e275610557c
Shreekar Ganapati Hegde <shreekarhegde9@gmail.com>
2021-05-06 20:25:00 +0530

    Added user token after login

--- backend/src/authentication.js
+++ backend/src/authentication.js
@@ -1,13 +1,36 @@
-const { AuthenticationService, JWTStrategy } = require('@feathersjs/authentication');
-const { LocalStrategy } = require('@feathersjs/authentication-local');
-const { expressOauth } = require('@feathersjs/authentication-oauth');
+const {
+  AuthenticationService,
+  JWTStrategy,
+} = require("@feathersjs/authentication");
+const { LocalStrategy } = require("@feathersjs/authentication-local");
+const { expressOauth } = require("@feathersjs/authentication-oauth");
+const { END_POINTS } = require("./api-end-points");
+const mongoose = require("mongoose");
 
-module.exports = app => {
+module.exports = (app) => {
   const authentication = new AuthenticationService(app);
 
-  authentication.register('jwt', new JWTStrategy());
-  authentication.register('local', new LocalStrategy());
+  authentication.register("jwt", new JWTStrategy());
+  authentication.register("local", new LocalStrategy());
 
-  app.use('/user/login', authentication);
+  app.use("/user/login", authentication, updateUserToken);
+
+  function updateUserToken(req, res, next) {
+    console.log("authenticated", res.data);
+    const userService = app.service(END_POINTS.user);
+    const patchQuery = {
+      _id: mongoose.Types.ObjectId(res.data.user._id),
+    };
+    const patchObj = {
+      token: res.data.accessToken,
+    };
+    userService
+      .patch(patchQuery, patchObj)
+      .then((patchRes) => {
+        console.log("patchRes", patchRes);
+        next();
+      })
+      .catch((err) => res.send({ err: err }));
+  }
   app.configure(expressOauth());
 };
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -6,19 +6,23 @@ const { Schema } = require("mongoose");
 
 // for more of what you can do here.
 module.exports = function (app) {
-  const modelName = 'user';
-  const mongooseClient = app.get('mongooseClient');
-  const schema = new mongooseClient.Schema({
-    email: { type: String, unique: true, lowercase: true },
-    password: { type: String },
-    isLoggedIn: { type: Boolean },
-    firstName: { type: String },
-    lastName: { type: String },
-    address: { type: String },
-    cartID: { type: Schema.Types.ObjectId, ref: 'cart' }
-  }, {
-    timestamps: true
-  });
+  const modelName = "user";
+  const mongooseClient = app.get("mongooseClient");
+  const schema = new mongooseClient.Schema(
+    {
+      email: { type: String, unique: true, lowercase: true },
+      password: { type: String },
+      isLoggedIn: { type: Boolean },
+      firstName: { type: String },
+      lastName: { type: String },
+      address: { type: String },
+      token: { type: String, unique: true },
+      cartID: { type: Schema.Types.ObjectId, ref: "cart" },
+    },
+    {
+      timestamps: true,
+    }
+  );
 
   // This is necessary to avoid model compilation errors in watch mode
   // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
@@ -26,5 +30,4 @@ module.exports = function (app) {
     mongooseClient.deleteModel(modelName);
   }
   return mongooseClient.model(modelName, schema);
-
 };
